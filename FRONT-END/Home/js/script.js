987ad977d43438ea767ea36cd1b089ccfc12c538
Yeferson Agudelo Castaño <agudelocastanoyeferson270@gmail.com>
2024-09-04 12:40:52 -0500

    Primer commit

--- FRONT-END/Home/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let toggler = document.querySelector('.navbar-toggler');
-    let navbarCollapse = document.querySelector('#navbarScroll');
-
-    toggler.addEventListener('click', function() {
-        navbarCollapse.classList.toggle('show');
-    });
-});
